c174af432febc460acd66f4550d3caddb8e23bf9
John <john@real-currents.com>
2021-11-01 15:43:08 -0400

    finish base leaflet reprex

--- 00_reprex-leaflet/Readme.md
+++ 00_reprex-leaflet/Readme.md
@@ -1,3 +1,1 @@
-This small Shiny application demonstrates Shiny's automatic UI updates. 
-
-Move the *Number of bins* slider and notice how the `renderPlot` expression is automatically re-evaluated when its dependant, `input$bins`, changes, causing a histogram with a new number of bins to be rendered.
+Simple performance test for plotting markers on a map.
--- 00_reprex-leaflet/app.R
+++ 00_reprex-leaflet/app.R
@@ -1,14 +1,27 @@
+library(magrittr)
 library(shiny)
-ui <- fluidPage(
-  selectInput("n", "N", 1:10),
-  plotOutput("plot")
+
+ui <- shiny::fluidPage(
+  shiny::selectInput("n", "N", 1:100),
+  leaflet::leafletOutput("mymap")
 )
+
 server <- function(input, output, session) {
-  output$plot <- renderPlot({
-    n <- as.numeric(input$n) * 2
-    plot(head(cars, n))
+  data(quakes)
+
+  output$mymap <- leaflet::renderLeaflet({
+    # Show user selected rows from the `quakes` dataset
+    m <- leaflet::leaflet(data = quakes[1:as.numeric(input$n),]) %>%
+      leaflet::addTiles() %>%  # Add default OpenStreetMap map tiles %>%
+      leaflet::addMarkers(~long, ~lat, popup = ~as.character(mag), label = ~as.character(mag)) %>%
+      leaflet::addAwesomeMarkers(lng=174.768, lat=-36.852, popup="The birthplace of R", icons <- leaflet::makeAwesomeIcon(
+        iconColor = "black",
+        markerColor = "red",
+        library = "fa"
+      ))
+    m  # Print the map
   })
 }
 
-shinyApp(ui, server)
+shiny::shinyApp(ui, server)
 
