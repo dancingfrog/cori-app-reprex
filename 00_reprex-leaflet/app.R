ef04d99c653a17101bdaeb47333d311cf8c83f66
John <john@real-currents.com>
2021-11-01 18:09:36 -0400

    expand leaflet reprex

--- 00_reprex-leaflet/app.R
+++ 00_reprex-leaflet/app.R
@@ -1,27 +1,57 @@
+library(leaflet)
 library(magrittr)
 library(shiny)
 
-ui <- shiny::fluidPage(
-  shiny::selectInput("n", "N", 1:100),
-  leaflet::leafletOutput("mymap")
+ui <- bootstrapPage(
+  tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
+  absolutePanel(top = 10, right = 10,
+  # fluidRow(
+  #   column(9,
+  #          tags$span("")
+  #   ),
+    column(3, class = "number-of-points",
+             selectInput("n", "Number of Points", 1:100),
+             tags$style(type = "text/css", ".number-of-points { margin-top: 10px; min-width: 200px; z-index: 1000 }")
+    )
+  # ),
+  ),
+  # fluidRow(
+  #   column(12,
+        leafletOutput("map", width = "100%", height = "100%")
+  #   )
+  # )
 )
 
 server <- function(input, output, session) {
-  data(quakes)
 
-  output$mymap <- leaflet::renderLeaflet({
-    # Show user selected rows from the `quakes` dataset
-    m <- leaflet::leaflet(data = quakes[1:as.numeric(input$n),]) %>%
-      leaflet::addTiles() %>%  # Add default OpenStreetMap map tiles %>%
-      leaflet::addMarkers(~long, ~lat, popup = ~as.character(mag), label = ~as.character(mag)) %>%
-      leaflet::addAwesomeMarkers(lng=174.768, lat=-36.852, popup="The birthplace of R", icons <- leaflet::makeAwesomeIcon(
-        iconColor = "black",
-        markerColor = "red",
-        library = "fa"
-      ))
-    m  # Print the map
+  # Reactive expression for the data subsetted to what the user selected
+  filteredData <- reactive({
+    quakes[1:as.numeric(input$n),]
+  })
+
+  observe({
+
+    awesomeIcons <- leaflet::awesomeIcons(
+      icon = 'circle',
+      iconColor = "black",
+      markerColor = "red",
+      library = "fa"
+    )
+
+    leafletProxy("map", data = filteredData()) %>%
+      leaflet::clearShapes() %>%
+      leaflet::clearMarkers() %>%
+      leaflet::addAwesomeMarkers(lng=174.768, lat=-36.852, popup="The birthplace of R", icons <- awesomeIcons) %>%
+      leaflet::addMarkers(~long, ~lat, popup = ~as.character(mag), label = ~as.character(mag), icons <- awesomeIcons)
+  })
+
+  output$map <- renderLeaflet({
+    leaflet(quakes) %>%
+      leaflet::addTiles() %>%
+      leaflet::clearShapes() %>%
+      leaflet::clearMarkers() %>%
+      leaflet::fitBounds(~min(long), ~min(lat), ~max(long), ~max(lat))
   })
 }
 
 shiny::shinyApp(ui, server)
-
